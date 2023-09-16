81030240e5d69a0e83eca60285205a8c64011d71
pck <3500146682@qq.com>
2023-09-16 22:52:46 +0800

    搭建 HTML 结构

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,4 @@
 <template>
-  <div>
-    导航栏 |
-    <router-link to="/">PCK</router-link> |
-    <router-link to="/2">PPCCKK</router-link>
-  </div>
-  <hr />
   <router-view />
 </template>
 
---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div class="topnav">
+      <div class="logo"></div>
+      <div class="menu"></div>
+    </div>
+    <div class="banner">
+      <h1>洋葱UI</h1>
+      <h2>一个贼厉害的 UI 框架</h2>
+      <p class="actions">
+        <a>GitHub</a>
+        <a>开始</a>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
