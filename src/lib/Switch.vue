22ff1b627cf40f742e92645a7fe1f84e9c716409
pck <3500146682@qq.com>
2023-09-18 23:17:41 +0800

    优化 css 代码

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <button :class="{checked: value}" @click="toggle"> <span></span> </button>
+    <button :class="{checked: value}" @click="toggle">
+      <span></span>
+    </button>
   </div>
 </template>
 
@@ -21,31 +23,20 @@ export default {
 <style lang="scss" scoped>
 $h: 22px;
 $h2: $h - 4px;
-button{
-  height: $h;
-  width: $h*2;
-  border: none;
-  background: grey;
-  border-radius: $h/2;
-  position: relative;
-}
-span{
-  position: absolute;
-  top: 2px;
-  left: 2px;
-  height: $h2;
-  width: $h2;
-  background:white;
-  border-radius: $h2 / 2;
-  transition: left 250ms;
-}
-button.checked > span {
-  left: calc(100% - #{$h2} - 2px);
-}
-button.checked {
-  background: blue;
-}
-button:focus {
-  outline: none;
+button {
+  height: $h; width: $h * 2; border: none; background: #bfbfbf; border-radius: $h/2; position: relative;
+  > span {
+    position: absolute; top: 2px; left: 2px; height: $h2; width: $h2; background: white; border-radius: $h2 / 2; transition: all 250ms;
+  }
+  &.checked { background: #1890ff;
+    > span { left: calc(100% - #{$h2} - 2px); }
+  }
+  &:focus { outline: none; }
+  &:active {
+    > span { width: $h2 + 4px; }
+  }
+  &.checked:active {
+    > span { width: $h2 + 4px; margin-left: -4px; }
+  }
 }
 </style>
