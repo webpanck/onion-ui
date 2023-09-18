492a81f0fdf1ce8a8c23d707620b56a9b945254f
pck <3500146682@qq.com>
2023-09-18 23:28:34 +0800

    创建 Button 组件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -2,4 +2,14 @@
   <div>
     Button 示例
   </div>
+  <h1>示例</h1>
+  <Button>PCK</Button>
 </template>
+
+<script lang="ts">
+import Button from '../lib/Button.vue'
+
+export default {
+  components: {Button}
+}
+</script>
