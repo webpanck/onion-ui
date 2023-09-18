360b95035fff49f358dd81ab4edfa27dc7ef1020
pck <3500146682@qq.com>
2023-09-19 00:37:51 +0800

    CSS最小影响原则

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <button :class="{checked: value}" @click="toggle">
+    <button class="onion-switch" :class="{'onion-checked': value}" @click="toggle">
       <span></span>
     </button>
   </div>
@@ -20,22 +20,22 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 $h: 22px;
 $h2: $h - 4px;
-button {
+.onion-switch {
   height: $h; width: $h * 2; border: none; background: #bfbfbf; border-radius: $h/2; position: relative;
   > span {
     position: absolute; top: 2px; left: 2px; height: $h2; width: $h2; background: white; border-radius: $h2 / 2; transition: all 250ms;
   }
-  &.checked { background: #1890ff;
+  &.onion-checked { background: #1890ff;
     > span { left: calc(100% - #{$h2} - 2px); }
   }
   &:focus { outline: none; }
   &:active {
     > span { width: $h2 + 4px; }
   }
-  &.checked:active {
+  &.onion-checked:active {
     > span { width: $h2 + 4px; margin-left: -4px; }
   }
 }
