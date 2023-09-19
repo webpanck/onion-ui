672d545fd4e176343eeb071d721e70846679abc9
pck <3500146682@qq.com>
2023-09-19 22:37:26 +0800

    使用 Teleport

--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,21 +1,23 @@
 <template>
   <template v-if="visible">
-    <div class="onion-dialog-overlay" @click="onClickOverlay"></div>
-    <div class="onion-dialog-wrapper">
-      <div class="onion-dialog">
-        <header>
-          <slot name="title" />
-          <span class="onion-dialog-close" @click="close"></span>
-        </header>
-        <main>
-          <slot name="content" />
-        </main>
-        <footer>
-          <Button level="main" @click="ok">OK</Button>
-          <Button @click="cancel">Cancel</Button>
-        </footer>
+    <Teleport to="body">
+      <div class="onion-dialog-overlay" @click="onClickOverlay"></div>
+      <div class="onion-dialog-wrapper">
+        <div class="onion-dialog">
+          <header>
+            <slot name="title" />
+            <span class="onion-dialog-close" @click="close"></span>
+          </header>
+          <main>
+            <slot name="content" />
+          </main>
+          <footer>
+            <Button level="main" @click="ok">OK</Button>
+            <Button @click="cancel">Cancel</Button>
+          </footer>
+        </div>
       </div>
-    </div>
+    </Teleport>
   </template>
 </template>
 
