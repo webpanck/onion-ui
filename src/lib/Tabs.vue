332b3352258c138695292cd932f07cbf38f11d05
pck <3500146682@qq.com>
2023-09-20 21:40:21 +0800

    实现下划线滑动

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="onion-tabs">
-    <div class="onion-tabs-nav">
+    <div class="onion-tabs-nav" ref="container">
       <div
         v-for="(t,index) in titles"
         :key="index"
         class="onion-tabs-nav-item"
         :class="{selected: t === selected}"
+        :ref="el => { if (el) navItems[index] = el }"
         @click="select(t)"
       >
         {{t}}
       </div>
+      <div class="onion-tabs-nav-indicator" ref="indicator"></div>
     </div>
     <div class="onion-tabs-content">
       <component
@@ -25,6 +27,7 @@
 
 <script lang="ts">
 import Tab from './Tab.vue'
+import {onMounted, onUpdated, ref} from 'vue'
 
 export default {
   props: {
@@ -33,6 +36,25 @@ export default {
     }
   },
   setup(props, context) {
+    const navItems = ref<HTMLDivElement[]>([])
+    const indicator = ref<HTMLDivElement>(null)
+    const container = ref<HTMLDivElement>(null)
+    const x = () => {
+      const divs = navItems.value
+      const result = divs.filter(div => div.classList.contains('selected'))[0]
+      const {width} = result.getBoundingClientRect()
+      indicator.value.style.width = width + 'px'
+      const {
+        left: left1
+      } = container.value.getBoundingClientRect()
+      const {
+        left: left2
+      } = result.getBoundingClientRect()
+      const left = left2 - left1
+      indicator.value.style.left = left + 'px'
+    }
+    onMounted(x)
+    onUpdated(x)
     const defaults = context.slots.default()
     defaults.forEach(tag => {
       if (tag.type !== Tab) {
@@ -45,7 +67,7 @@ export default {
     const select = (title: string) => {
       context.emit('update:selected', title)
     }
-    return {defaults, titles, select}
+    return {defaults, titles, select, navItems, indicator, container}
   }
 }
 </script>
@@ -59,6 +81,7 @@ $border-color: #d9d9d9;
     display: flex;
     color: $color;
     border-bottom: 1px solid $border-color;
+    position: relative;
     &-item {
       padding: 8px 0;
       margin: 0 16px;
@@ -69,6 +92,15 @@ $border-color: #d9d9d9;
       &.selected {
         color: $blue;
       }
+    }
+    &-indicator {
+      position: absolute;
+      height: 3px;
+      background: $blue;
+      left: 0;
+      bottom: -1px;
+      width: 100px;
+      transition: all 250ms;
     }
   }
   &-content {
