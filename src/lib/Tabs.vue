a60f40fd52db40061038fb41391b4e433207a3d8
pck <3500146682@qq.com>
2023-09-20 20:52:34 +0800

    使用 CSS 切换内容

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,10 +1,24 @@
 <template>
   <div class="onion-tabs">
     <div class="onion-tabs-nav">
-      <div class="onion-tabs-nav-item" v-for="(t,index) in titles" :key="index">{{t}}</div>
+      <div
+        v-for="(t,index) in titles"
+        :key="index"
+        class="onion-tabs-nav-item"
+        :class="{selected: t === selected}"
+        @click="select(t)"
+      >
+        {{t}}
+      </div>
     </div>
     <div class="onion-tabs-content">
-      <component class="onion-tabs-content-item" v-for="(c,index) in defaults" :is="c" :key="index" />
+      <component
+        v-for="(c,index) in defaults"
+        :is="c"
+        :key="index"
+        class="onion-tabs-content-item"
+        :class="{selected: c.props.title === selected}"
+      />
     </div>
   </div>
 </template>
@@ -13,6 +27,11 @@
 import Tab from './Tab.vue'
 
 export default {
+  props: {
+    selected: {
+      type: String
+    }
+  },
   setup(props, context) {
     const defaults = context.slots.default()
     defaults.forEach(tag => {
@@ -23,7 +42,10 @@ export default {
     const titles = defaults.map(tag => {
       return tag.props.title
     })
-    return {defaults, titles}
+    const select = (title: string) => {
+      context.emit('update:selected', title)
+    }
+    return {defaults, titles, select}
   }
 }
 </script>
@@ -51,6 +73,12 @@ $border-color: #d9d9d9;
   }
   &-content {
     padding: 8px 0;
+    &-item {
+      display: none;
+      &.selected {
+        display: block;
+      }
+    }
   }
 }
 </style>
