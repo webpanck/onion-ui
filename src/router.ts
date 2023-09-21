637a6c704770433c0e7ebdae10b69a0e5d916891
pck <3500146682@qq.com>
2023-09-21 20:49:57 +0800

    创建路由

--- src/router.ts
+++ src/router.ts
@@ -7,6 +7,10 @@ import ButtonDemo from './components/ButtonDemo.vue'
 import DialogDemo from './components/DialogDemo.vue'
 import TabsDemo from './components/TabsDemo.vue'
 
+import Intro from './views/Intro.vue';
+import GetStarted from './views/GetStarted.vue'
+import Install from './views/Install.vue'
+
 const history = createWebHashHistory()
 export const router = createRouter({
   history: history,
@@ -17,6 +21,9 @@ export const router = createRouter({
       component: Doc,
       children: [
         { path: "", component: DocDemo },
+        { path: "intro", component: Intro },
+        { path: "get-started", component: GetStarted },
+        { path: "install", component: Install },
         { path: "switch", component: SwitchDemo },
         { path: "button", component: ButtonDemo },
         { path: "dialog", component: DialogDemo },
