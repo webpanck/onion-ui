d92af392d2a58f4567bf29fef12fd1a665da60d6
pck <3500146682@qq.com>
2023-09-25 23:55:05 +0800

    优化官网，并将 topnav 的 z-index 调成 20

--- src/router.ts
+++ src/router.ts
@@ -3,7 +3,6 @@ import {createRouter, createWebHashHistory} from 'vue-router'
 import Home from './views/Home.vue'
 import Doc from './views/Doc.vue'
 import SwitchDemo from './components/SwitchDemo.vue'
-import DocDemo from './components/DocDemo.vue'
 import ButtonDemo from './components/ButtonDemo.vue'
 import DialogDemo from './components/DialogDemo.vue'
 import TabsDemo from './components/TabsDemo.vue'
@@ -21,7 +20,7 @@ export const router = createRouter({
       path: "/doc",
       component: Doc,
       children: [
-        { path: "", component: DocDemo },
+        { path: "", redirect: '/doc/intro' },
         { path: "intro", component: md('intro') },
         { path: "get-started", component: md('get-started') },
         { path: "install", component: md('install') },
