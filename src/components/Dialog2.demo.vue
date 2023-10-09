ff4f6bd08179a5d3d994a42a2352b105e73b5012
pck <3500146682@qq.com>
2023-10-09 23:05:34 +0800

    从 index 引入组件

--- src/components/Dialog2.demo.vue
+++ src/components/Dialog2.demo.vue
@@ -8,14 +8,12 @@
 </template>
 
 <script lang="ts">
-import Button from '../lib/Button.vue'
+import {Button, openDialog} from "../lib/index"
 import {
   ref,
   h
 } from 'vue'
-import {
-  openDialog
-} from '../lib/openDialog'
+
 export default {
   components: {
     Button
--- src/markdown/install.md
+++ src/markdown/install.md
@@ -3,13 +3,13 @@
 打开终端运行下列命令：
 
 ```
-npm install onion-ui
+npm install onion-ui-panck
 ```
 
 或
 
 ```
-yarn add onion-ui
+yarn add onion-ui-panck
 ```
 
 下一节：[开始使用](#/doc/get-started)
